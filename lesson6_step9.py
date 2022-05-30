2dd9be65051ffc0baef8a54d9edd54a544752f65
artyrshur <artyrshurshin@gmail.com>
2022-05-30 13:30:08 +0300

    get attribute from picture without value

---
+++ lesson6_step9.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import math
+import time
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id('treasure')
+    x = x_element.get_attribute("valuex")
+    y = calc(int(x))
+
+
+    input = browser.find_element_by_id('answer')
+    input.send_keys(y)
+
+    checkbox = browser.find_element_by_id("robotCheckbox")
+    checkbox.click()
+
+    radioB = browser.find_element_by_id("robotsRule")
+    radioB.click()
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    time.sleep(10)
+    browser.quit()
+
+
