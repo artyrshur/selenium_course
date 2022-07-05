e800e95a34bef089b801bb1a435352c082f30951
artyrshur <artyrshurshin@gmail.com>
2022-07-05 22:50:24 +0300

    select new home with price between 1000000 and 3000000

--- etagi_test.py
+++ etagi_test.py
@@ -22,18 +22,13 @@ try:
     browser.implicitly_wait(2)
     checkbox5 = browser.find_element_by_xpath('//*[@id="wARPj2Cdgv"]/div[2]/div/div[1]/div[4]/div[2]/button').click()
     browser.implicitly_wait(2)
+    check = browser.find_element_by_xpath('//*[@id="wARPj2Cdgv"]/div[2]/div/div[1]/div[4]/div[2]/div/div/button[4]').click()
     checkbox6 = browser.find_element_by_xpath('//*[@id="wARPj2Cdgv"]/div[2]/div/div[1]/div[4]/div[2]/div/div/button[1]').click()
+    check1 = browser.find_element_by_xpath('//*[@id="wARPj2Cdgv"]/div[2]/div/div[1]/div[5]/div[2]/div/div/button[1]').click()
     browser.implicitly_wait(2)
     btn2 = browser.find_element_by_xpath('//*[@id="wARPj2Cdgv"]/div[2]/div/div[2]/div[3]/a').click()
-    browser.implicitly_wait(2)
-    search = browser.find_element_by_xpath('//*[@id="object-list"]/div/div').click()
-    browser.implicitly_wait(3)
-    button = browser.find_element_by_css_selector('#etagiTabs > div._9xwA7._2DZKJ > div > div > nav > ul > li:nth-child(4)')
-    button.click()
-    #browser.execute_script("window.scrollBy(0, 330);", button)
-    #btn3 = browser.find_element_by_css_selector('#content > div > div > div > div._11n5y > div._1PJ8R > div > div.pXm1d > a > button').click()
-    #input6.send_keys('9177777777')
-    #btn3 = browser.find_element_by_xpath('//*[@id="enterKeyOrder"]/button/span').click()
+
+
 finally:
     time.sleep(10)
     browser.quit()
